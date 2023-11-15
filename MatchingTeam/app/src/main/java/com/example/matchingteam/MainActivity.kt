e8d72fd2cec6aaf69bf373f453a049984f9e9e0b
희망 <122812652+w1shope@users.noreply.github.com>
2023-11-15 11:11:32 +0900

    회원가입 API 구현

--- MatchingTeam/app/src/main/java/com/example/matchingteam/MainActivity.kt
+++ MatchingTeam/app/src/main/java/com/example/matchingteam/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.matchingteam
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.matchingteam.databinding.ActivityMainBinding
 import retrofit2.Call
@@ -12,40 +14,21 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val textViewResult = binding.textViewResult
-        var retrofit = Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8080")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        val registerBtn: Button = binding.buttonOpenRegisterForm
+        registerBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
-        val jsonPlaceHolderApi = retrofit.create(RestApi::class.java)
-        val call: Call<List<User>> = jsonPlaceHolderApi.getPost()
-        call.enqueue(object : Callback<List<User>> {
-            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
-                if (!response.isSuccessful) {
-                    textViewResult.text = "Code: ${response.code()}"
-                    return
-                }
-                val users: List<User>? = response.body()
-                users?.forEach { user ->
-                    var content = ""
-                    content += "ID: ${user.id}\n"
-                    content += "Email: ${user.email}\n"
-                    content += "Name: ${user.name}\n"
-                    content += "Password: ${user.password}\n"
-                    content += "Development: ${user.development}\n"
-                    content += "Introduce: ${user.introduce}\n\n"
-                    textViewResult.append(content)
-                }
-            }
-
-            override fun onFailure(call: Call<List<User>>, t: Throwable) {
-                textViewResult.setText(t.message)
-            }
-        })
+        val loginBtn: Button = binding.buttonOpenLoginForm
+        loginBtn.setOnClickListener {
+            val intent: Intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- MatchingTeam/app/src/main/java/com/example/matchingteam/User.kt
+++ MatchingTeam/app/src/main/java/com/example/matchingteam/User.kt
@@ -1,10 +1,22 @@
 package com.example.matchingteam
 
-data class User(
-    val id: Long = 0,
-    val email: String = "",
-    val name: String = "",
-    val password: String = "",
-    val development: String = "",
-    val introduce: String = ""
-)
+import com.google.gson.annotations.SerializedName
+import retrofit2.http.FormUrlEncoded
+
+class User {
+    @SerializedName("email")
+    private lateinit var email: String
+    @SerializedName("name")
+    private lateinit var name: String
+    @SerializedName("password")
+    private lateinit var password: String
+    @SerializedName("development")
+    private lateinit var development: String
+
+    constructor(email: String, name: String, password: String, development: String) {
+        this.email = email
+        this.name = name
+        this.password = password
+        this.development = development
+    }
+}
