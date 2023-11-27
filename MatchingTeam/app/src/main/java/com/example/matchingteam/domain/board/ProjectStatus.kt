241ea2dc4c46f6405a81ea92f330f87d4634c738
희망 <122812652+w1shope@users.noreply.github.com>
2023-11-28 02:58:04 +0900

    spring server upload

--- MatchingTeam/app/src/main/java/com/example/matchingteam/domain/board/ProjectStatus.kt
+++
@@ -1,5 +0,0 @@
-package com.example.matchingteam.domain.board
-
-enum class ProjectStatus {
-    RECRUITING, COMPLETE
-}
--- MatchingTeam/app/src/main/java/com/example/matchingteam/dto/board/BoardStatusDto.kt
+++
@@ -1,8 +0,0 @@
-package com.example.matchingteam.dto.board
-
-import com.google.gson.annotations.SerializedName
-
-data class BoardStatusDto(
-    @SerializedName("title") val title: String,
-    @SerializedName("content") val content: String,
-)
